3a03864c3e5815934ba6b584972dae5e4a510ee3
HIkmatillo <m.khikmatillo@gmail.com>
2017-04-15 21:48:58 -0400

    again

--- src/Tester/Tester.java
+++ src/Tester/Tester.java
@@ -5,6 +5,12 @@ public class Tester {
 	public static void main(String[] args) {
 		System.out.println("Hikmatillo");
         //comment
+		int next,sum=0;
+		for(next=0;next<=10;next++){
+			sum=sum+next;
+			System.out.println("Sum up to "+next+" is "+sum);
+		}
 	}
+	
 
 }
